22e327e1d5bb986b5eb6b3a77ff14afa53193afd
Supriya Sangolli <108995675+Supriyars@users.noreply.github.com>
2025-07-16 11:14:05 +0530

    Create Problem-3.java

---
+++ Problem-3.java
@@ -0,0 +1,16 @@
+// Problem-3.java
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        int limit = a % 2 == 0 ? a - 1 : a;
+
+        for (int i = 1, count = 0; count < limit; i += 2, count++) {
+            System.out.print(i);
+            if (count < limit - 1) System.out.print(", ");
+        }
+    }
+}
